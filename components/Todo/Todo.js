fb97ec6ec670bb1ce5943b8c16926cba53320123
laura <lauraeguda@gmail.com>
2021-11-05 17:02:01 +0100

    update

--- components/Todo/Todo.js
+++ components/Todo/Todo.js
@@ -4,6 +4,7 @@ import styles from './Todo.module.css';
  function Todo() {
   const [name, setName] = useState('')
   const [list, setList] = useState([])
+  const [filteredlist, setFilteredList] = useState([])
   const [purpleset, setPurpleset] = useState(false)
   const [greenset, setGreenset] = useState(false)
   
@@ -13,14 +14,14 @@ import styles from './Todo.module.css';
       const greenlist = list.filter((item)=>{
         return   item.color == incomingcolor;
        })
-      setList(greenlist)
+       setFilteredList(greenlist)
       setGreenset(!greenset)
       return
     } else{
       const purpleset = list.filter((item)=>{
         return   item.color == incomingcolor;
       })
-      setList(purpleset)
+      setFilteredList(purpleset)
       setPurpleset(!purpleset)
     }
   }
@@ -30,6 +31,7 @@ import styles from './Todo.module.css';
     if(name){
       const newItem = {id: new Date().toString(), title: name, color: 'purple' }
     setList([...list, newItem])
+    setFilteredList([...list, newItem])
     setName('')
     console.log(newItem)
     console.log(list)
@@ -43,6 +45,7 @@ import styles from './Todo.module.css';
     if(name){
       const newItem = {id: new Date().toString(), title: name, color: 'green' }
     setList([...list, newItem])
+    setFilteredList([...list, newItem])
     setName('')
     console.log(newItem)
     console.log(list)
@@ -57,9 +60,9 @@ import styles from './Todo.module.css';
       Today, {date}
      </div>
      <div className={styles.showtasksection}>
-      <div className={styles.task}>{purpleset||greenset? null : `showing ${list.length} task${list.length > 1? 's': ''}`}
-                            {purpleset? `filtering and showing ${list.length} task${list.length > 1? 's': ''}`: null} 
-                            {greenset? `filtering and showing ${list.length} task${list.length > 1? 's': ''}` : null}
+      <div className={styles.task}>{purpleset||greenset? null : `showing ${filteredlist.length} task${filteredlist.length > 1? 's': ''}`}
+                            {purpleset? `filtering and showing ${filteredlist.length} task${filteredlist.length > 1? 's': ''}`: null} 
+                            {greenset? `filtering and showing ${filteredlist.length} task${filteredlist.length > 1? 's': ''}` : null}
                            </div>
         <div>
           <button className={styles.colorbuttonOne} onClick={()=> handleClick('purple')}></button>
@@ -67,9 +70,9 @@ import styles from './Todo.module.css';
         </div>
      </div>
      {
-       list.length > 0 ?
+       filteredlist.length > 0 ?
        
-       list.map(({id, title, color})=> (
+       filteredlist.map(({id, title, color})=> (
          <div className={styles.individualtasks}>
             <input type={styles.checkbox} className={styles.checkinput}/>
            <p key={id}>{title}</p>
